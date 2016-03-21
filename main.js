9a5e7a91313970187694c867d8b89806a1f637a5
chrissschin <solischris92@gmail.com>
2016-03-20 21:52:33 -0700

    added php files

--- main.js
+++ main.js
@@ -3,4 +3,26 @@ $(document).ready(function() {
 
 	$('body').fadeIn(750);
 
+
+	/*
+		Contact form
+	*/
+	$('form').submit(function(e) {
+		e.preventDefault();
+		var postdata = $(this).serialize();
+		$.ajax({
+			type: 'POST',
+			url: 'contact.php',
+			data: postdata,
+			dataType: 'json',
+			success: function(json) {
+
+				if(json.nameMessage == '' && json.emailMessage == '' && json.subjectMessage == '' && json.messageMessage == '') {
+					$('.submit').text("Thank You.");
+				}
+
+			}
+		});
+	});
+
 });
